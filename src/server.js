14ce9d76e6984fdc8edc86e1d48c160b243ad908
kang-mincheol <aaa6315@gmail.com>
2022-11-15 23:55:41 +0900

    data type 별 분기처리

--- src/server.js
+++ src/server.js
@@ -22,7 +22,13 @@ wss.on("connection", (socket) => {
     console.log("Connected to Browser ✅");
     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
     socket.on("message", (message) => {
-        sockets.forEach((aSocket) => aSocket.send(message.toString()));
+        const parsed = JSON.parse(message);
+        if(parsed.type === "new_message") {
+            sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+        } else if(parsed.type === "nickname") {
+            console.log(parsed.payload);
+        }
+        
     });
 });
 
