d410f98b34303552787eed97df1205051aaeb6fc
kang-mincheol <aaa6315@gmail.com>
2022-11-25 23:51:51 +0900

    socket io rooms

--- src/server.js
+++ src/server.js
@@ -14,8 +14,14 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+    });
     socket.on("enter_room", (roomName, done) => {
-        console.log(roomName);
+        console.log(socket.id);
+        console.log(socket.rooms);
+        socket.join(roomName);
+        console.log(socket.rooms);
         setTimeout(() => {
             done("Hello form the backend");
         }, 15000);
